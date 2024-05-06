6cb12f1949ee947250062b54e7137ce7050b8fac
Kunal Singh <ks820177@gmail.com>
2024-05-06 15:12:10 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { PixiPlugin } from "gsap/all";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, PixiPlugin);
+
 function Home() {
+  const heroRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.from(heroRef.current, {
+      // opacity: 0,
+      duration: 1.5,
+      delay: 3,
+      filter: "blur(15px)",
+    });
+  });
+
   return (
-    <div className="flex justify-center items-center h-full my-[1000px]">
-      Home
+    <div
+      ref={heroRef}
+      className="flex flex-col justify-center items-center h-screen"
+    >
+      <h1 className="text-3xl font-black">Kunal Singh!</h1>
+      <p className="text-xl font-bold">A passionate Full Stack Developer</p>
     </div>
   );
 }
