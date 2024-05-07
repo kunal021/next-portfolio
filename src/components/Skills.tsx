6d50eab5aba4d040b7aae1baf725be2c70ad53b7
Kunal Singh <ks820177@gmail.com>
2024-05-07 09:55:24 +0530

    update

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,10 +1,40 @@
+"use client";
+import { useEffect, useRef } from "react";
+
 function Skills() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const options: IntersectionObserverInit = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.6,
+    };
+
+    const callback: IntersectionObserverCallback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          videoRef.current?.play();
+        } else {
+          videoRef.current?.pause();
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    if (videoRef.current) observer.observe(videoRef.current);
+
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      if (videoRef.current) observer.unobserve(videoRef.current);
+    };
+  }, []);
   return (
     <div>
       <video
+        ref={videoRef}
         loop
         muted
-        autoPlay
         className="h-screen w-fit absolute object-cover"
       >
         <source src="/skills/cards-video.webm" type="video/webm" />
